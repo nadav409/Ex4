67dd7dce92f929af92e3e161b9c9b1ab31487642
nadav409 <nadav409@gmail.com>
2025-02-23 15:52:23 +0200

    34 version

--- src/Ex2SheetTest.java
+++ src/Ex2SheetTest.java
@@ -28,7 +28,7 @@ class Ex2SheetTest {
         assertEquals("16.0", sheet.value(0, 2));
         sheet.set(0, 3, "=-5+10");
         assertEquals("5.0", sheet.value(0, 3));
-        sheet.set(0,4,"=a3*5");
+        sheet.set(0, 4, "=a3*5");
         assertEquals("25.0", sheet.value(0, 4));
     }
 
@@ -59,6 +59,7 @@ class Ex2SheetTest {
         assertFalse(sheet.validCell(null));
         assertFalse(sheet.validCell("naa34"));
     }
+
     @Test
     void testAdvancedValidCell() {
         assertTrue(sheet.advancedValidCell("A1"));
@@ -112,10 +113,10 @@ class Ex2SheetTest {
         sheet.set(2, 0, "20");
         sheet.set(3, 0, "=sum(B0:C0)");
         assertEquals("30.0", sheet.value(3, 0));
-        sheet.set(3,0,"string");
+        sheet.set(3, 0, "string");
         sheet.set(4, 0, "=sum(B0:D0)");
         assertEquals(Ex2Utils.FUNC_ERR, sheet.value(4, 0));
-        sheet.set(3,0,"");
+        sheet.set(3, 0, "");
         assertEquals("30.0", sheet.value(4, 0));
         sheet.set(0, 0, "=sum(A1,b2)");
         assertEquals(Ex2Utils.FUNC_ERR, sheet.value(0, 0));
@@ -127,14 +128,15 @@ class Ex2SheetTest {
         sheet.set(2, 0, "20");
         sheet.set(3, 0, "=Max(B0:C0)");
         assertEquals("20.0", sheet.value(3, 0));
-        sheet.set(3,0,"string");
+        sheet.set(3, 0, "string");
         sheet.set(4, 0, "=max(B0:D0)");
         assertEquals(Ex2Utils.FUNC_ERR, sheet.value(4, 0));
-        sheet.set(3,0,"");
+        sheet.set(3, 0, "");
         assertEquals("20.0", sheet.value(4, 0));
         sheet.set(0, 0, "=MAX(A1)");
         assertEquals(Ex2Utils.FUNC_ERR, sheet.value(0, 0));
     }
+
     @Test
     void testMinFunction() {
         sheet.set(1, 0, "10");
@@ -146,6 +148,7 @@ class Ex2SheetTest {
         sheet.set(1, 1, "=d0");
         assertEquals("2.0", sheet.value(1, 1));
     }
+
     @Test
     void testAverageFunction() {
         sheet.set(0, 0, "10");
@@ -168,6 +171,7 @@ class Ex2SheetTest {
         sheet.set(4, 4, "=5++3");
         assertEquals(null, sheet.eval(4, 4));
     }
+
     @Test
     void testIsForm() {
         assertTrue(sheet.isForm("5+3"));
@@ -182,6 +186,7 @@ class Ex2SheetTest {
         assertFalse(sheet.isForm(""));
         assertFalse(sheet.isForm(null));
     }
+
     @Test
     void testDepth() {
         sheet.set(0, 0, "5");
@@ -212,5 +217,78 @@ class Ex2SheetTest {
         assertEquals(4, depth3[4][0]);
         assertEquals(5, depth3[5][0]);
     }
+
+    @Test
+    void testifCondition() {
+        String formula = "=if(A1>5, 10, 20)";
+        assertEquals("A1>5", sheet.ifCondition(formula));
+        String formula1 = "=if(A1==5, 10, 20)";
+        assertEquals("A1==5", sheet.ifCondition(formula1));
+        String formula2 = "=if((2+3)*8+a0>20, 10, 20)";
+        assertEquals("(2+3)*8+a0>20", sheet.ifCondition(formula2));
+    }
+    @Test
+    void testIfTrue() {
+        String formula = "=if(A1>5,B1+3,C1-4)";
+        assertEquals("B1+3", sheet.ifTrue(formula));
+        String formula1 = "=if(A1>5,=if(B1<3,7,9),=C1-4)";
+        assertEquals("=if(B1<3,7,9)", sheet.ifTrue(formula1));
+    }
+    @Test
+    void testIfFalse() {
+        String formula = "=if(A1>5,10,20)";
+        assertEquals("20", sheet.ifFalse(formula));
+        String formula1 = "=if(A1>5,B1+3,C1-4)";
+        assertEquals("C1-4", sheet.ifFalse(formula1));
+        String formula2 = "=if(A1>5,40,=if(B1<3,7,9))";
+        assertEquals("=if(B1<3,7,9)", sheet.ifFalse(formula2));
+    }
+    @Test
+    void testEvaluateCondition() {
+        assertTrue(sheet.evaluateCondition("=if(5>2,10,20)"));
+        assertFalse(sheet.evaluateCondition("=if(5<2,10,20)"));
+        assertTrue(sheet.evaluateCondition("=if(5!=3,5,20)"));
+        assertFalse(sheet.evaluateCondition("=if(5!=5,5,20)"));
+        sheet.set(0, 0, "10");
+        sheet.set(1, 0, "20");
+        assertTrue(sheet.evaluateCondition("=if(a0<b0,5,20)"));
+        assertFalse(sheet.evaluateCondition("=if(A0>B0,5,20)"));
+        sheet.set(0, 0, "10");
+        sheet.set(1, 0, "20");
+        assertTrue(sheet.evaluateCondition("=if(A0+5<B0,5,20)"));
+        assertFalse(sheet.evaluateCondition("=if(A0*3>B0*5,5,20)"));
+    }
+    @Test
+    void testIfFunction() {
+        sheet.set(0, 0, "=if(5>2,100,200)");
+        assertEquals("100.0", sheet.value(0, 0));
+        sheet.set(1, 0, "10");
+        sheet.set(2, 0, "20");
+        sheet.set(0, 0, "=if(B0>C0,100,200)");
+        assertEquals("200.0", sheet.value(0, 0));
+        sheet.set(0, 0, "=if(10>5,=if(2<3,100,200),300)");
+        assertEquals("100.0", sheet.value(0, 0));
+        sheet.set(0, 0, "=if(abc,1,2)");
+        assertEquals(Ex2Utils.ERRWRONG_IF, sheet.value(0, 0));
+    }
+    @Test
+    void testValidIfCorrect() {
+        assertTrue(sheet.validIf("=if(A1>5,10,20)"));
+        assertTrue(sheet.validIf("=if(B1==C1,yes,no)"));
+        assertFalse(sheet.validIf("=IF(A1>5, 10)"));
+        assertFalse(sheet.validIf("=if(A1>5,,20)"));
+        assertFalse(sheet.validIf("=if(B1==C1,yes ,no)"));
+        assertFalse(sheet.validIf("=if(B1==C1,=2>5,no)"));
+    }
+    @Test
+    void testCountOccurrencesBasic() {
+        assertEquals(2, sheet.countOccurrences("=if(A1>5, 10, 20), if(A2<5, 30, 40)", "if"));
+        assertEquals(0, sheet.countOccurrences("=A1+B1", "if"));
+        assertEquals(3, sheet.countOccurrences("if if if", "if"));
+    }
+
+
+
+
 
 }
